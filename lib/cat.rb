78f4d5301ed2dcda20ef786589f7916865a6ea73
Andy Adams <andrewdcadams@gmail.com>
2020-08-04 17:27:26 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -7,23 +7,6 @@ class Cat
   
   def initialize(new_name, new_owner)
     @name = new_name
-    # found = false
-    # found_owner = nil
-    # Owner.all.each do |owner_obj|
-    #   if owner_obj.name == new_owner
-    #     found_owner = owner_obj
-    #     found = true
-    #     break
-    #   end
-    # end
-    # if found == true 
-    #   @owner = found_owner
-    # else 
-    #   created_owner = Owner.new(new_owner)
-    #   @owner = created_owner
-    #   #binding.pry
-    # end
-    
     @owner = new_owner
     @owner.cats << self
     @mood = "nervous"
